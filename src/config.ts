762f423d029501930bcadbc452409c12ea811197
Matt Mcgillivray <m.mcgillivray@outlook.com>
2023-02-15 21:14:30 +0800

    capitalised title

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Sub 50k";
+export const SITE_TITLE = "SUB 50k";
 export const SITE_DESCRIPTION =
   "Artists on Spotify with less than 50k monthly listeners.";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
