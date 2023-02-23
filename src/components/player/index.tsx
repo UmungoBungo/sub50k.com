35dd52b42c265b885c85c2295c05e674a5a10213
--edit <m.mcgillivray@outlook.com>
2023-02-23 09:29:30 +0800

    player stroke effect working

--- src/components/player/index.tsx
+++ src/components/player/index.tsx
@@ -1,8 +1,8 @@
 import { Player, PlayerRef } from "@remotion/player";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { MyComposition } from "./myComposition";
 import {
-    PlayIcon, PauseIcon
+    PlayIcon, PauseIcon, BackwardIcon
 } from '@heroicons/react/24/solid/index.js';
 
 {/* 
@@ -17,30 +17,67 @@ const RemotionPlayer: React.FC = () => {
     const [isPlaying, setIsPlaying] = useState(false);
     const fps = 30;
 
+    useEffect(() => {
+        const { current } = playerRef;
+        if (!current) {
+            return;
+        }
+
+        const pauseListener = () => {
+            setIsPlaying(false)
+        };
+
+        const playListener = () => {
+            setIsPlaying(true)
+        };
+        current.addEventListener("pause", pauseListener);
+        current.addEventListener("play", playListener)
+        return () => {
+            current.removeEventListener("pause", pauseListener);
+            current.removeEventListener("play", playListener)
+        };
+    }, []);
+
     function handleTogglePlay(event: React.MouseEvent<HTMLButtonElement>) {
         if (!playerRef.current) return
         event.stopPropagation();
         if (isPlaying) {
             playerRef.current.pause();
-            setIsPlaying(false);
         } else {
             playerRef.current.play();
-            setIsPlaying(true);
         }
     }
+
+    function handleReset(event: React.MouseEvent<HTMLButtonElement>) {
+        if (!playerRef.current) return
+        event.stopPropagation();
+        playerRef.current.seekTo(0);
+    }
+
     return (
-        <div>
-            <Player
-                ref={playerRef}
-                component={MyComposition}
-                durationInFrames={fps * durationInSeconds}
-                compositionWidth={1}
-                compositionHeight={1}
-                style={{ display: 'none' }}
-                inputProps={{ trackName: "conformist_inhale.m4a" }}
-                fps={fps}
-            />
+        <div className="flex not-prose items-center">
+            {/* is playing state needs to reference playerRef, as pause button remains in place when audio is finished */}
             <button type='button' className='bg-primary hover:ring-primary focus:border-code focus:ring-primary mr-2 flex h-10 w-10 items-center justify-center rounded border border-gray-500 hover:ring-1 hover:ring-offset-0 focus:outline-none text-primary-main' onClick={handleTogglePlay}>{isPlaying ? <PauseIcon className="h-5 w-5" /> : <PlayIcon className="h-5 w-5" />}</button>
+
+
+            <div className="relative group">
+                <Player
+                    ref={playerRef}
+                    component={MyComposition}
+                    durationInFrames={fps * durationInSeconds}
+                    compositionWidth={500}
+                    compositionHeight={43}
+                    inputProps={{
+                        filename: "conformist.m4a", trackName: "Conformist"
+                    }}
+                    fps={fps}
+                />
+                <div className="absolute inset-0 group-hover:opacity-100 opacity-0 transition-opacity">
+                    <div className="h-10 flex items-end">
+                        <button type='button' className='bg-primary hover:ring-primary focus:border-code focus:ring-primary mr-2 flex h-5 w-5 items-center justify-center rounded border border-gray-500 hover:ring-1 hover:ring-offset-0 focus:outline-none text-primary-main' onClick={handleReset}><BackwardIcon className="h-3 w-3" /></button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/player/myComposition.tsx
+++ src/components/player/myComposition.tsx
@@ -1,8 +1,46 @@
-import { Audio, staticFile, useVideoConfig, interpolate } from "remotion";
+import { Audio, staticFile, useVideoConfig, interpolate, AbsoluteFill, useCurrentFrame } from "remotion";
 // conformist_inhale
-export const MyComposition = ({ trackName }: { trackName: string }) => {
+export const MyComposition = ({ filename, trackName }: { filename: string, trackName: string }) => {
   const { durationInFrames } = useVideoConfig();
-  return (<Audio src={staticFile(trackName)} volume={(f) =>
-    interpolate(f, [0, 30, durationInFrames - 30, durationInFrames], [0, 1, 1, 0], { extrapolateLeft: "clamp", extrapolateRight: "clamp" })
-  } />);
+  const frame = useCurrentFrame();
+
+  function toHHMMSS(sec_num: number) {
+    var minutes = Math.floor((sec_num - Math.floor(sec_num / 3600) * 3600) / 60);
+    var seconds = sec_num - Math.floor(sec_num / 3600) * 3600 - minutes * 60;
+
+    let minutesString = minutes < 10 ? `0${minutes}` : minutes.toString();
+    let secondsString = seconds < 10 ? `0${seconds}` : seconds.toString();
+    return `${minutesString}:${secondsString}`;
+  }
+
+  return (
+    <div className="flex">
+      <Audio src={staticFile(filename)} volume={(f) =>
+        interpolate(f, [0, 30, durationInFrames - 30, durationInFrames], [0, 1, 1, 0], { extrapolateLeft: "clamp", extrapolateRight: "clamp" })
+      } />
+      <div className="relative h-10 flex w-fit">
+        <div className="absolute inset-0 flex items-end">
+          <h2 className="text-3xl font-bold color-bg-body" style={{
+            color: 'rgb(var(--color-bg-body))',
+            'textShadow': '-1px -1px 0 rgb(var(--color-text-body)), 1px -1px 0 rgb(var(--color-text-body)), -1px 1px 0 rgb(var(--color-text-body)), 1px 1px 0 rgb(var(--color-text-body));'
+          }}>{trackName}</h2>
+        </div>
+        <div className="absolute inset-0 flex items-end">
+          <h2 className="text-3xl font-bold color-bg-body overflow-hidden" style={{
+            width: `${frame / durationInFrames * 100}%`,
+            backgroundColor: 'rgb(var(--color-bg-body))',
+            color: 'rgb(var(--color-primary-main))',
+          }}>{trackName}</h2>
+        </div>
+        <h2 className="text-3xl font-bold h-10 invisible">{trackName}</h2>
+      </div>
+      <div className="ml-2 text-default flex items-end font-mono text-xs font-medium leading-5">
+        <span className="flex w-10 justify-center">{toHHMMSS(Math.round(frame / 30))}</span>
+        <span className="flex w-4 justify-center text-center">/</span>
+        <span className="flex w-10 justify-center">
+          {toHHMMSS(durationInFrames / 30)}
+        </span>
+      </div>
+    </div>
+  );
 };
