98268e3d8459a515b363311fc5ce7dd8f4f9da6d
Matt McGillivray <m.mcgillivray@outlook.com>
2023-03-28 10:07:22 +0800

    sorting RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,12 @@ import {getCollection} from 'astro:content';
 import {SITE_TITLE, SITE_DESCRIPTION, SITE_URL} from '../config';
 
 export async function get() {
-	const artists = await getCollection('artist');
+	const allPosts = await getCollection('artist');
+
+	const sortedPosts = allPosts
+		.sort((a, b) => b.data.publishDate.valueOf() - a.data.publishDate.valueOf())
+		.filter((post) => !post.data.draft);
+
 	let baseUrl = SITE_URL;
 	baseUrl = baseUrl.replace(/\/+$/g, '');
 
@@ -16,7 +21,7 @@ export async function get() {
 		site: baseUrl,
 		// Array of `<item>`s in output xml
 		// See "Generating items" section for examples using content collections and glob imports
-		items: artists.map((artist) => ({
+		items: sortedPosts.map((artist) => ({
 			title: artist.data.name,
 			pubDate: artist.data.publishDate,
 			description: artist.data.description,
