83074f366c740add05745cb6f86a4ad874fd2b23
Matt McGillivray <m.mcgillivray@outlook.com>
2023-08-31 20:01:03 +0800

    upgrade to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,35 +8,39 @@
   If you don't know your website URL yet, don't worry about this
   and leave it empty or use localhost URL. It won't break anything.
 */
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
+import {defineConfig} from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
 const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://sub50k.com";
+const LIVE_URL = 'https://sub50k.com';
 // this is the astro command your npm script runs
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
+const SCRIPT = process.env.npm_lifecycle_script || '';
+const isBuild = SCRIPT.includes('astro build');
 let BASE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
-  BASE_URL = LIVE_URL;
+	BASE_URL = LIVE_URL;
 }
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: SERVER_PORT
-  },
-  site: BASE_URL,
-  integrations: [sitemap(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), react(), mdx(), image()]
+	server: {
+		port: SERVER_PORT,
+	},
+	site: BASE_URL,
+	integrations: [
+		sitemap(),
+		tailwind({
+			config: {
+				applyBaseStyles: false,
+			},
+		}),
+		react(),
+		mdx(),
+	],
 });
